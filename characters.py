cc45719dab414be385411f4763d1673aa9267fd7
Roko Lovrecek <you@example.com>
2025-09-17 23:41:09 +0200

    GIT

--- characters.py
+++ characters.py
@@ -1,7 +1,36 @@
 '''
-@ASSESSME.USERID: userID
-@ASSESSME.AUTHOR: author, list of authors
+@ASSESSME.USERID: rl3805
+@ASSESSME.AUTHOR: Roko Lovrecek
 @ASSESSME.DESCRIPTION: Assignment 2.2
 @ASSESSME.ANALYZE: YES
 '''
 
+def add_chars(char1,char2):
+    val1 = ord(char1)
+    val2 = ord(char2)
+
+    sum = val1 + val2
+    print("Sum of a and b = "+str(sum)+ "\nCharacter = " + chr(sum)+"\n")
+
+    
+
+
+def subtract_chars(c1,c2):
+    value1 = ord(c1)
+    value2 = ord(c2)
+
+    subtraction = value1 - value2
+    print("Difference of "+c1+" and "+c2+" = "+str(subtraction)+ "\nCharacter = " + chr(subtraction)+"\n")
+
+
+def main():
+    add_chars("a","b")
+    subtract_chars("b","a")
+
+
+if __name__ == "__main__":
+    main()
+
+
+#odd characters or nothing appears when the sum exceeds the range of ASCII(0-127)
+#ord() and chr() are only able to hold one character; if more are entered syntax error occurs
--- scope.py
+++ scope.py
@@ -4,3 +4,29 @@
 @ASSESSME.DESCRIPTION: Assignment 2.2
 @ASSESSME.ANALYZE: YES
 '''
+
+STRING_GLOBAL: str="Hello World"
+INT_GLOBAL: int=1215
+FLOAT_GLOBAL: float=21.43
+
+def print_param():
+    print(STRING_GLOBAL)
+    print(INT_GLOBAL)
+    print(FLOAT_GLOBAL)
+
+def print_local():
+    a = "local variable."
+    print(a)
+
+def print_which():
+    STRING_GLOBAL: int=123
+    print(STRING_GLOBAL)
+
+def main():
+    print_param()
+    print_local()
+    print_which()
+
+
+if __name__ == "__main__":
+    main()
